76c73d648cacc04fa0bed88045df4c40feb60dc9
PEKODEV <108638658+Midrooms@users.noreply.github.com>
2024-02-29 08:40:34 -0500

    Express Lite 1.1
    -Launch game with server option
    -Updated look
    -Updated LC version

--- AstralExpress-lite.bat
+++ AstralExpress-lite.bat
@@ -1,10 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
+
 :menu
-echo Welcome to Astral Express    1.0
+:: Check if config file exists and load game path
+if exist gamepath.txt (
+    set /p gamepath=<gamepath.txt
+    set "option1=Toggle launch game with server (Currently ON)"
+) else (
+    set gamepath=
+    set "option1=Toggle launch game with server (Currently OFF)"
+)
+
+:: Remove quotes from option1
+for /f "delims=" %%A in ("%option1%") do set option1=%%~A
+
+echo Welcome to Astral Express 1.0
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo 1^) Start server ^& enable proxy
+echo 1^) Start server
 echo 2^) Build/repair server
-echo 3^) Stop proxy ^& exit
+echo 3^) %option1%
+echo 4^) Stop proxy ^& exit
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo AstralPlus by Midrooms
 echo LunarCore by Melledy
@@ -13,7 +28,8 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set /p op=Select: 
 if "%op%"=="1" goto start
 if "%op%"=="2" goto build
-if "%op%"=="3" start /min cmd /k "cd "%CD%\Resources" && proxy.bat 2" && exit
+if "%op%"=="3" goto toggle
+if "%op%"=="4" start /min cmd /k "cd "%CD%\Resources" && proxy.bat 2" && exit
 
 cls
 goto menu
@@ -21,10 +37,27 @@ goto menu
 :start
 start /min cmd /k "cd "%CD%\Resources" && proxy.bat 1"
 start cmd /k "cd "%CD%\Resources" && java -jar LunarCore.jar"
+if not "!gamepath!"=="" (
+    timeout /t 10 /nobreak
+    start "" !gamepath!
+)
 cls
 goto menu
 
 :build
 start cmd /c "cd "%CD%\Resources" && gradlew jar && exit"
+cls
+goto menu
+
+:toggle
+if not "!gamepath!"=="" (
+    del gamepath.txt
+    set gamepath=
+) else (
+    set /p "gamepath=Paste full game path here: "
+    if not "!gamepath!"=="" (
+        echo !gamepath! > gamepath.txt
+    )
+)
 cls
 goto menu
